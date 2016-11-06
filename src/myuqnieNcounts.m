85c2a81023d000d7a521eb73bff9bde38b0a6edc
Hyunwoo Kim <kimbellw@gmail.com>
2016-11-06 15:36:50 -0600

    Unique elements and their counts.

---
+++ src/myuqnieNcounts.m
@@ -0,0 +1,15 @@
+function [U, C] = myuqnieNcounts(data)
+%MYUQNIENCOUNTS returns the unique elements from data and C is the counts
+%of the elements.
+%
+%
+%   See Also: UNIQUE
+
+%   $ Hyunwoo J. Kim $  $ 2016/11/06 15:27:57 (CST) $
+
+    [U, ~, ic]  = unique(data);
+    C =zeros(length(U),1); % C is the counts of elements
+    for i=1:length(U)
+        C(i) = sum(ic==i);
+    end
+end
